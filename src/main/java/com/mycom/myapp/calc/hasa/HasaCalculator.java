487f5cce0e1556ae80de99b2cf837c89afb7029a
tjdqls3607 <tjdqls3124@gmail.com>
2025-03-31 11:36:00 +0900

    Spring - Has a

--- src/main/java/com/mycom/myapp/calc/hasa/HasaCalculator.java
+++ src/main/java/com/mycom/myapp/calc/hasa/HasaCalculator.java
@@ -1,5 +1,6 @@
 package com.mycom.myapp.calc.hasa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 // Calculator 객체 필요
@@ -7,8 +8,36 @@ import org.springframework.stereotype.Component;
 @Component
 public class HasaCalculator {
 	// Spring DI 를 사용하지 않는 경우
-	Calculator calculator = new Calculator();
+//	Calculator calculator = new Calculator();
 	
+	// Spring DI #1 field injection
+//	@Autowired
+//	Calculator calculator;
+	
+	
+	// Spring DI #2 setter injection
+//	Calculator calculator;
+//
+//
+//	@Autowired
+//	public void setCalculator(Calculator calculator) {
+//		this.calculator = calculator;
+//	}
+	
+	//Spring DI #3 constriuctor injection
+	// @Autowired 필요X
+	Calculator calculator;
+	
+//	@Autowired
+	public HasaCalculator (Calculator calculator) {
+		super();
+		this.calculator = calculator;
+	}
+	
+	
+
+
+
 	public int add (int n1, int n2) {
 		System.out.println("HasaCalculator add()");
 		return calculator.add(n1, n2);
