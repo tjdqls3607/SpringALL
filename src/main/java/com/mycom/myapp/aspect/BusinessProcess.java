533ebeb70dedaa3967b718b9ce9745df5c78d9fc
tjdqls3607 <tjdqls3124@gmail.com>
2025-03-31 15:31:45 +0900

    SpringAOP-sub

--- src/main/java/com/mycom/myapp/aspect/BusinessProcess.java
+++ src/main/java/com/mycom/myapp/aspect/BusinessProcess.java
@@ -1,5 +1,4 @@
 package com.mycom.myapp.aspect;
-
 import org.springframework.stereotype.Component;
 
 // 개발자가 집중 개발하는 Business logic 을 구현하는 클래스
@@ -7,24 +6,22 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class BusinessProcess {
-	
-	public void bp() {
-		System.out.println("BusinessProcess : bp()!!");
-	}
-	
-	public void no_bp() {
-		System.out.println("BusinessProcess : no_bp()!!");
-	}
-	
-	public int int_bp() {
-		System.out.println("BusinessProcess : int_bp()!!");
-		return 0;
-	}
-	
-	public int int_String_bp(String s1, int i) {
-		System.out.println("BusinessProcess : int_String_bp()!!");
-		return 0;
-	}
-
-
+    
+    public void bp() {
+        System.out.println("BusinessProcess : bp() !!");
+    }
+    
+    public void no_bp() {
+        System.out.println("BusinessProcess : no_bp() !!");
+    }
+    
+    public int int_bp() {
+        System.out.println("BusinessProcess : int_bp() !!");
+        return 0;
+    }
+    
+    public int int_String_bp(String s1, int i) {
+        System.out.println("BusinessProcess : int_String_bp() !!");
+        return 0;
+    }
 }
---
+++ src/main/java/com/mycom/myapp/aspect/sub/SubBusinessProcess.java
@@ -0,0 +1,10 @@
+package com.mycom.myapp.aspect.sub;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SubBusinessProcess {
+	
+    public void bp() {
+        System.out.println("SubBusinessProcess : bp() !!");
+    }
+}
