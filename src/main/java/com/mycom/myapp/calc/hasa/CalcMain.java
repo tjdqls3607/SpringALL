2fe6c23e6cb2415a7ab1333325e0415d1486fd0c
tjdqls3607 <tjdqls3124@gmail.com>
2025-03-31 11:26:41 +0900

    Spring-has a

--- src/main/java/com/mycom/myapp/calc/hasa/CalcMain.java
+++ src/main/java/com/mycom/myapp/calc/hasa/CalcMain.java
@@ -9,7 +9,7 @@ public class CalcMain {
 		
 		// Spring DI를 통한 객체 생성
 		// main() 에서 Spring Framework 의 환경
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("xml/calc-annotation.xml"); // 설정 xml 문서
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("xml/hasa-calc-annotation.xml"); // 설정 xml 문서
 		HasaCalculator hasaCalculator = (HasaCalculator)context.getBean("hasaCalculator");	// name 기준 객체를 DI 
 		System.out.println(hasaCalculator.add(3,7));
 		context.close();
