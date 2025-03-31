0c7e83d3a68f0fd5463db3fe024c1c77c2fc5ac9
tjdqls3607 <tjdqls3124@gmail.com>
2025-03-31 14:22:50 +0900

    SpringAOP-aspect

--- src/main/java/com/mycom/myapp/aspect/LogAspect.java
+++ src/main/java/com/mycom/myapp/aspect/LogAspect.java
@@ -1,6 +1,12 @@
 package com.mycom.myapp.aspect;
 
-// Logging 을 담당하는 Aspect
-public class LogAspect {
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
+// Logging 을 담당하는 Aspect
+@Component	// 객체가 필요한 시점에 Spring 이 DI
+@Aspect	//aspectj 라이브러리에 의해 AOP 로 동작 (<aop:aspectj-autoproxy></aop:aspectj-autoproxy> )
+public class LogAspect {
+	
+	// logging 을 위한 객체 필요 
 }
