912399966d5afe18c2df8b9c5082239324134fb5
tjdqls3607 <tjdqls3124@gmail.com>
2025-03-31 14:14:28 +0900

    StringAOP-Aspect

--- src/main/java/com/mycom/myapp/aspect/AspectMain.java
+++ src/main/java/com/mycom/myapp/aspect/AspectMain.java
@@ -1,9 +1,22 @@
 package com.mycom.myapp.aspect;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AspectMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("xml/aspect.xml");
+		BusinessProcess bp = (BusinessProcess) context.getBean("businessProcess");
+		bp.bp();
+		System.out.println("--------------------------------------------------");
+		bp.no_bp();
+		System.out.println("--------------------------------------------------");
+		bp.int_bp();
+		System.out.println("--------------------------------------------------");
+		bp.int_String_bp("s1", 0);
+		System.out.println("--------------------------------------------------");
+		
+		context.close();
 
 	}
 
