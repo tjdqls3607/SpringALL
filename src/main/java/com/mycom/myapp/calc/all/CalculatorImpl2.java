e7d73e331f9caf07a0e63eafdd14a02350559c74
tjdqls3607 <tjdqls3124@gmail.com>
2025-03-31 13:21:01 +0900

    Spring-manyimplements

--- src/main/java/com/mycom/myapp/calc/all/CalculatorImpl2.java
+++ src/main/java/com/mycom/myapp/calc/all/CalculatorImpl2.java
@@ -2,13 +2,13 @@ package com.mycom.myapp.calc.all;
 
 import org.springframework.stereotype.Component;
 
-@Component("aaa")
-public class CalculatorImpl implements Calculator {
+@Component("bbb")
+public class CalculatorImpl2 implements Calculator {
 	
     @Override
     public int add(int n1, int n2) {
     	
-        System.out.println("CalculatorImpl add()");
+        System.out.println("CalculatorImpl2 add()");
         return n1 + n2;
     }
 }
--- src/main/java/com/mycom/myapp/calc/all/HasaCalculator.java
+++ src/main/java/com/mycom/myapp/calc/all/HasaCalculator.java
@@ -1,6 +1,6 @@
 package com.mycom.myapp.calc.all;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 // Calculator 객체 필요
@@ -11,7 +11,7 @@ public class HasaCalculator {
 	//Spring DI #3 constriuctor injection (spring 추천방식)
 	Calculator calculator;	// interface 를 implements 한 객체가 DI
 	
-	public HasaCalculator (Calculator calculator) {
+	public HasaCalculator (@Qualifier("aaa") Calculator calculator) {
 		super();
 		this.calculator = calculator;
 	}
