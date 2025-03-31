049208c4099df77430102781aee66616a113ff65
tjdqls3607 <tjdqls3124@gmail.com>
2025-03-31 16:15:17 +0900

    SpringDI 를 활용한 오늘의 포켓몬(100) 마리중 한마리 랜덤 출력

---
+++ src/main/java/com/mycom/myapp/SpringWorkShopApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWorkShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWorkShopApplication.class, args);
+	}
+
+}
