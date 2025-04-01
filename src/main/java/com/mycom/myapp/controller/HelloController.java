cf482da8d2ffd77070725d09046d59116df5ce31
tjdqls3607 <tjdqls3124@gmail.com>
2025-04-01 10:55:41 +0900

    SpringBootMVC-jsp

---
+++ src/main/java/com/mycom/myapp/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.mycom.myapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// 클라이언트의 request 를 받고 처리
+@Controller // <- 이 annotation 이 있는 클래스의 메소드들의 mapping 정보를 스프링이 수집해서 Handler Mapping 자료구조에 저장
+public class HelloController {
+	
+	// get /hello
+	@GetMapping("/hello")
+	public String hello() {
+		System.out.println("Hello...!!!");
+		return "hello";	//jsp WEB-INF/jsp/hello.jsp
+	}
+}
