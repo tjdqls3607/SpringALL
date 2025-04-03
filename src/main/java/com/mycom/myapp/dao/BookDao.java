93704216e4147cd45f238d1b0bb116e39ccda7fd
tjdqls3607 <tjdqls3124@gmail.com>
2025-04-03 11:23:53 +0900

    SpringBootMVCDBAjaxMybatis

--- src/main/java/com/mycom/myapp/dao/BookDao.java
+++ src/main/java/com/mycom/myapp/dao/BookDao.java
@@ -2,9 +2,12 @@ package com.mycom.myapp.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.mycom.myapp.dto.BookDto;
 
 //book crud
+@Mapper
 public interface BookDao {
 	
 	List<BookDto> listBook();
