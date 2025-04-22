1050facbcaa62c53869e4f8d3b99ef0fb05eb382
tjdqls3607 <tjdqls3124@gmail.com>
2025-04-22 11:22:02 +0900

    스프링 lombok

--- SpringBootJPABasicCrudFind/src/main/java/com/mycom/myapp/lombok/Test.java
+++ SpringBootJPABasicCrudFind/src/main/java/com/mycom/myapp/lombok/Test.java
@@ -1,6 +1,7 @@
 package com.mycom.myapp.lombok;
 
 public class Test {
+	
 	public static void main(String[] args) {
 		// Builder 패턴 적용
 		EmpDto2 empDto2 = EmpDto2.builder()
@@ -13,5 +14,10 @@ public class Test {
 							.build();
 		
 		System.out.println(empDto2);
+		
 	}
+	
 }
+
+// Emp <-Entity, EmpDto 등 Emp 와 별개로 만들어야 하는가???
+// Entity 와 별개의 Entity 를 표현하는 Dto 를 만드는게 일반적
