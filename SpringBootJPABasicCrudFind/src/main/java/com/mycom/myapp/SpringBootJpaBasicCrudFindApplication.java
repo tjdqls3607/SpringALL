3fe5f981d5e252974a5f97228f659c5db8a5387c
tjdqls3607 <tjdqls3124@gmail.com>
2025-04-22 10:31:55 +0900

    스프링부트 JPA

---
+++ SpringBootJPABasicCrudFind/src/main/java/com/mycom/myapp/SpringBootJpaBasicCrudFindApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaBasicCrudFindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaBasicCrudFindApplication.class, args);
+	}
+
+}
