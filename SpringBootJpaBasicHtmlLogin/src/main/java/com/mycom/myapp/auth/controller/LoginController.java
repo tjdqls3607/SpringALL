793e92ef8e9f80364bc136d37875d5e44cf50bc9
tjdqls3607 <tjdqls3124@gmail.com>
2025-04-23 13:32:33 +0900

    스프링 html 로그인

--- SpringBootJpaBasicHtmlLogin/src/main/java/com/mycom/myapp/auth/controller/LoginController.java
+++ SpringBootJpaBasicHtmlLogin/src/main/java/com/mycom/myapp/auth/controller/LoginController.java
@@ -1,5 +1,34 @@
 package com.mycom.myapp.auth.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mycom.myapp.auth.service.LoginService;
+import com.mycom.myapp.user.dto.UserResultDto;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class LoginController {
 
+	private final LoginService loginService;
+	
+	@PostMapping("/login")
+	public UserResultDto login(
+			@RequestParam("email") String email,
+			@RequestParam("password") String password,
+			HttpSession session
+			) {
+		UserResultDto userResultDto = loginService.login(email, password); 
+		
+		if (userResultDto.getResult().equals("success")) {
+			session.setAttribute("userDto", userResultDto.getUserDto());
+		}
+		return userResultDto;
+	}
 }
