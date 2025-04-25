23b0b2f3f0637e0413eae315d5f4866ddd224aea
tjdqls3607 <tjdqls3124@gmail.com>
2025-04-25 15:02:02 +0900

    스프링 JPQL

--- SpringBootJpaBasicJpqlDto/src/main/java/com/mycom/myapp/controller/OrdersController.java
+++ SpringBootJpaBasicJpqlDto/src/main/java/com/mycom/myapp/controller/OrdersController.java
@@ -1,10 +1,12 @@
 package com.mycom.myapp.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mycom.myapp.dto.OrdersDto;
 import com.mycom.myapp.entity.Orders;
 import com.mycom.myapp.service.OrdersService;
 
@@ -23,9 +25,23 @@ public class OrdersController {
 //		return ordersService.listOrder();
 //	}
 	
-	
+	// #1
+	// Controller 에서 Entity -> DTO 변경 후, DTO 를 JSON 으로 변환, 응답
 	@GetMapping("/listorders")
-	public List<Orders> listOrders() {
-		return ordersService.listOrder();
+	public List<OrdersDto> listOrders() {
+		List<Orders> ordersList =  ordersService.listOrder();
+		List<OrdersDto> ordersDtoList = new ArrayList<>();
+		
+		ordersList.forEach(orders -> {
+			OrdersDto ordersDto = 
+					OrdersDto.builder()
+					.id(orders.getId())
+					.orderQuantity(orders.getOrderQuantity())
+					.orderDate(orders.getOrderDate())
+					.build();
+			ordersDtoList.add(ordersDto);
+		});
+		
+		return ordersDtoList;
 	}
 }
