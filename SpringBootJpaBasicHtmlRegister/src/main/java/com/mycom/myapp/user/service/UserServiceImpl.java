0a12d354891bdacbba61de578053f689408dbbec
tjdqls3607 <tjdqls3124@gmail.com>
2025-04-25 13:32:57 +0900

    스프링 회원가입

--- SpringBootJpaBasicHtmlRegister/src/main/java/com/mycom/myapp/user/service/UserServiceImpl.java
+++ SpringBootJpaBasicHtmlRegister/src/main/java/com/mycom/myapp/user/service/UserServiceImpl.java
@@ -27,7 +27,7 @@ public class UserServiceImpl implements UserService {
 	private final UserRoleRepository userRoleRepository;
 
 	@Override
-	@Transactional
+//	@Transactional
 	public UserResultDto insertUser(User user) {
 		UserResultDto userResultDto = new UserResultDto();
 
@@ -48,13 +48,15 @@ public class UserServiceImpl implements UserService {
 //			User savedUser = userRepository.save(user);
 
 			// #2-2 userRole 객체를 save o -> 영속화 o
-//		UserRole userRole = new UserRole();
-//		userRole.setName("role_test");
-//		List<UserRole> userRoles = List.of(userRole);
-//		user.setUserRoles(userRoles);
-//		userRoleRepository.save(userRole);	// 위와 다른 부분
-//		User savedUser = userRepository.save(user);
+		UserRole userRole = new UserRole();
+		userRole.setName("role_test");
+		List<UserRole> userRoles = List.of(userRole);
+		user.setUserRoles(userRoles);
+		userRoleRepository.save(userRole);	// 위와 다른 부분
+		User savedUser = userRepository.save(user);
 
+		userRoleRepository.flush();
+		userRepository.flush();
 			// #3. transaction + #1 상황
 //		UserRole userRole = userRoleRepository.findByName("ROLE_CUSTOMER");
 //		List<UserRole> userRoles = List.of(userRole);
@@ -90,12 +92,12 @@ public class UserServiceImpl implements UserService {
 			// @Transactional 상황에서는 TransactionAspect 가 관여하고, Proxy 객체를 통해 우리의 코드를
 			// 대신 호출하고 최종적인 예외가 발생하지 않으면 TransactionAspect 가 commit() 을 수행하는데
 			// 이 과정에서 예외가 발생하므로 우리 코드 밖에서 생기는 예외를 우리코드에서 catch 하지 못하는 상황
-			UserRole userRole = new UserRole();
-			userRole.setName("role_test");
-			List<UserRole> userRoles = List.of(userRole);
-			user.setUserRoles(userRoles);
-			User savedUser = userRepository.save(user);
-			userRepository.flush();
+//			UserRole userRole = new UserRole();
+//			userRole.setName("role_test");
+//			List<UserRole> userRoles = List.of(userRole);
+//			user.setUserRoles(userRoles);
+//			User savedUser = userRepository.save(user);
+//			userRepository.flush();
 			
 			
 			userResultDto.setResult("success");
