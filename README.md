fc2a2ed9855bb8a010963173efcada0bc2ea93ae
tjdqls3607 <tjdqls3124@gmail.com>
2025-05-02 11:02:15 +0900

    Spring Security

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SpringALL
+# SpringSecurity
---
+++ SpringBootSecurity_1/src/main/java/com/mycom/myapp/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.mycom.myapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    // 요청에 대해서 모두 허락
+//    @Bean
+//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .authorizeHttpRequests(request -> request.anyRequest().permitAll())    // 권한에 대해서 http request를 어떻게 처리할지 (모든요청에 대해서 허가)
+//                .build();
+//    }
+
+    // 요청에 대해서 모두 인증 받아야됨
+    // 로그인 페이지를 통한 인증
+//    @Bean
+//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .authorizeHttpRequests( request -> request.anyRequest().authenticated() )    // 권한에 대해서 http request를 어떻게 처리할지 (전부다 인증을 받아야함)
+//                .formLogin( Customizer.withDefaults() ) // 아무런 설정 없는 경우와 동일
+//                .build();
+//    }
+
+    //  /,  index.html 모두 허락 나머지는 모두 인증
+    // 로그인 페이지를 통한 인증
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(
+                    request -> {
+                        request.requestMatchers("/", "/index.html").permitAll();
+                        request.anyRequest().authenticated();
+                    }
+                )    // 권한에 대해서 http request를 어떻게 처리할지 ( /,  index.html 모두 허락 나머지는 모두 인증)
+                .formLogin( Customizer.withDefaults() ) // 아무런 설정 없는 경우와 동일
+                .build();
+    }
+}
