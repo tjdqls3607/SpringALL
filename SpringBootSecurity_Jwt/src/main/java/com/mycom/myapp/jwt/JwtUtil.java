c8e1b1c876642bd94127d1a7badf4ab62e26b69d
tjdqls3607 <tjdqls3124@gmail.com>
2025-05-07 10:32:50 +0900

    Spring security_jwt

--- SpringBootSecurity_Jwt/src/main/java/com/mycom/myapp/jwt/JwtUtil.java
+++ SpringBootSecurity_Jwt/src/main/java/com/mycom/myapp/jwt/JwtUtil.java
@@ -1,8 +1,31 @@
 package com.mycom.myapp.jwt;
 
+import io.jsonwebtoken.Jwts;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 
 // jwt 생성 검증
 @Component
+@RequiredArgsConstructor
 public class JwtUtil {
+
+    @Value("${myapp.jwt.secret}")
+    private String secretKeyStr;
+    private SecretKey secretKey;
+    private final long tokenValidDuration = 1000L * 60 * 60 * 24;
+
+    @PostConstruct
+    private void init() {
+        System.out.println(secretKeyStr);
+        secretKey = new SecretKeySpec(secretKeyStr.getBytes(StandardCharsets.UTF_8),
+                Jwts.SIG.HS256.key().build().getAlgorithm()
+        );
+        System.out.println(secretKey);
+    }
 }
